ca2ad926fbd53eff6650dead10f375f0e4d45ec8
partho2589 <parthomalo615@.com>
2022-03-28 00:38:12 +0600

    add component cardgroups

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Cartgroup from './component/Cartgroup/Cartgroup';
+
+function App() {
+  return (
+    <div className="App">
+     <Cartgroup></Cartgroup>
+    </div>
+  );
+}
+
+export default App;
